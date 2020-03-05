bec158e1ba0d5202f92f573b543c6f87c8ae012d
Crystal Broach <crysland@gmail.com>
2020-03-04 21:07:57 -0600

    Add project files.

---
+++ MyFavoriteThings/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyFavoriteThings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("These are a few of my favorite things:");
+        }
+    }
+}
