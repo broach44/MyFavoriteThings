bbdc8151856ffaee8c2fbdb48d9c603be3ca43dc
Crystal Broach <crysland@gmail.com>
2020-03-07 12:48:23 -0600

    added favorite animals

--- MyFavoriteThings/Animals.cs
+++ MyFavoriteThings/Animals.cs
@@ -6,5 +6,35 @@ namespace MyFavoriteThings
 {
     class Animals
     {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Type { get; set; }
+        public bool IsMale { get; set; }
+        public bool IsSpayedOrNeutered { get; set; }
+
+        public Animals(string name, string type)
+        {
+            Name = name;
+            Type = type;
+        }
+
+        public void VisitVet()
+        {
+            if (!IsSpayedOrNeutered)
+            {
+                Console.WriteLine($"{Name} needs to go to the vet to be spayed/neutered.");
+            }
+            else
+            {
+                Console.WriteLine($"{Name} can no longer reproduce, yay!");
+            }
+        }
+
+        public void AboutPet()
+        {
+            var gender = (IsMale) ? "male" : "female";
+            Console.WriteLine($"{Name} is a {Age} year old {gender} {Type}");
+        }
     }
+
 }
