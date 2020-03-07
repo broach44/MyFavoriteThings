cb9956a71788034aa64e2b3dc9ccdb816228dc82
Crystal Broach <crysland@gmail.com>
2020-03-07 13:16:57 -0600

    fav vacay complete

--- MyFavoriteThings/Vacation.cs
+++ MyFavoriteThings/Vacation.cs
@@ -6,5 +6,24 @@ namespace MyFavoriteThings
 {
     class Vacation
     {
+        public int DaysUntil { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+
+        public Vacation(string name, string location)
+        {
+            Name = name;
+            Location = location;
+        }
+
+        public void FinalCountdown()
+        {
+            Console.WriteLine($"There are {DaysUntil} days until our {Name} to {Location}");
+        }
+
+        public void Dreaming()
+        {
+            Console.WriteLine($"I would really like to travel to {Location}");
+        }
     }
 }
