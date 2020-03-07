597c5fec1608ee967b5509002096e442fa25275b
Crystal Broach <crysland@gmail.com>
2020-03-07 12:19:46 -0600

    created bands

--- MyFavoriteThings/Program.cs
+++ MyFavoriteThings/Program.cs
@@ -7,6 +7,10 @@ namespace MyFavoriteThings
         static void Main(string[] args)
         {
             Console.WriteLine("These are a few of my favorite things:");
+
+            var band1 = new Bands("Coheed and Cambria", "Nashville");
+            band1.Perform();
+            band1.Record();
         }
     }
 }
